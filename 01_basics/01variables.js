4a2c5d7730c699c788de51122b6e00fcb7dba7cb
printishan10 <ishanjr022@gmail.com>
2026-06-15 00:34:39 +0000

    Added variables and datatypes files

---
+++ 01_basics/01variables.js
@@ -0,0 +1,22 @@
+let acc_id = 10
+const acc_name = "ish"
+var acc_emai= "isjsn@"
+acc_close = 1
+
+let acc_sum;  // js prints this as undefined when u just declare the var and not assign any value 
+
+// acc_name = "shrey"  cant change const 
+console.log(acc_name);
+/*
+we can change all other let , var and acc_close all exept const which cant be changed 
+*/
+
+/*
+then if let and var are same whtts diff 
+earlier js didnt had scope so people changed var easily n there was problem so let came people
+dont use var now only let and const 
+*/
+
+
+
+// node 01_basics/01variables.js // 
---
+++ 01_basics/02datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all js code as new version 
+
+// alert(3+3) //in browser it would run but here in node.js it dosent 
+
+// for documentation of js go to mdn //
+
+//primitive data types 
+/*
+number 
+bigint
+string ""
+boolean 
+null = means empty u can define null  ex- let temp = null 
+undefined = when u dont declare any value its undefined  ex let temp;
+symbol = used for unique
+*/
+
+// objects 
+
+console.log(typeof undefined);  //undefined 
+console.log(typeof null);      //object
+
+
+
+
+
+//node 01_basics/02datatypes.js
