8f80fd5dab7574469292ba450f3b87f791e6ce63
printishan10 <ishanjr022@gmail.com>
2026-06-22 19:41:14 +0000

    add comparisons notes

--- 01_basics/03conversion_operation.js
+++ 01_basics/03conversion_operation.js
@@ -14,3 +14,44 @@ console.log(valueInNum) // so basically js converted "33abc" to a number data ty
 
 
 //node 01_basics/03conversion_operation.js
+
+
+
+
+// *********************OPERATIONS**************************//
+//console.log(2+2)
+//console.log(2**2)   -means 2^2
+
+
+//console.log(1 + "2")   -12     If one value is a string, JavaScript converts the other value to a string and joins them.
+//console.log("1" + "2") -12
+//console.log("1" + 2 + 3)  -123  JavaScript evaluates + from left to right. so str + no is str then str + no is str so ans is str 
+//console.log(1 + 2 + "3")  -33  no + no is no then no plus str is str so ans is str 
+
+
+
+// THESE ARE THE RULES OF CONVERSION DURING ARITHMETIC 
+// String + Anything → String   true + "1" = true1   (string has highest priority during +)
+
+// true  → 1     ex - true + 2 = 3  
+// false → 0      flase + 2 = 2 
+
+// null → 0       null + 5 = 5
+
+// undefined → NaN    undefined + 5 = NaN
+
+// Numeric String + with - * / → Number  "5" - 2 = 3    (Numeric Strings convert to Numbers (for -, *, /))
+
+// Non-Numeric String → NaN    "hii" - 2 = NaN          (Non-Numeric Strings become NaN)
+
+
+
+
+// console.log(+true)  -1      The + before a value is called the unary plus operator. It  convert the value into a number. it dosent add
+// console.log(true)   - true 
+// console.log(true+)   ❌ Error   Because + needs a value on both sides when used as addition.
+
+//+"33"      // 33
+
+// let num1 , num2 , num3
+// num1 = num2 = num3 = 2+ 2   - 4    JavaScript works right to left for assignment (=). first num3 = 4 then num2 then num1
---
+++ 01_basics/04comparisons.js
@@ -0,0 +1,17 @@
+// console.log("2" > 1)  - true  js automatically converts 2 to number 
+// console.log("02" > 1)
+
+// console.log(null>=0); - true   For comparison operators (>, <, >=, <=) JavaScript converts null to a number which is 0 so 0>=0 true
+// console.log(null>0);  - false  For comparison operators (>, <, >=, <=), JavaScript converts null to a number which is 0 
+// console.log(null==0); - false  For equality check(==)  null == undefined  thats why null == 0 is  false but null == undefined is true 
+
+
+// STRICT CHECK - (===) not only checks value but the data types also
+// console.log("2" === 2); -false 
+// console.log("2" == 2);  -true 
+
+
+
+
+
+//   node 01_basics/04comparisons.js
